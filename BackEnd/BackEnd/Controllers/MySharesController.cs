e3f1047094e07652e12a4593f00a78b146cabfea
Fernando Neirot <fernando.neirot@hotmail.com>
2022-01-18 19:03:11 -0300

    agrego controller y dto para acciones

---
+++ BackEnd/BackEnd/Controllers/MySharesController.cs
@@ -0,0 +1,31 @@
+﻿using BackEnd.DTOs;
+using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BackEnd.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MySharesController : ControllerBase
+    {
+        // GET: api/<UserController>
+        [HttpGet]
+        [EnableCors("AllowOrigin")]
+        public IActionResult GetSharesByUser(string mail)
+        {
+            //Generar servicio para consultar a la DB
+            return Ok();
+        }
+        [HttpPost]
+        [EnableCors("AllowOrigin")]
+        public IActionResult Add(ShareDTO obj)
+        {
+            //Generar servicio para consultar a la DB
+            return Ok();
+        }
+
+    }
+}
---
+++ BackEnd/BackEnd/DTOs/ShareDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace BackEnd.DTOs
+{
+    public class ShareDTO
+    {
+        public string Symbol { get; set; }
+        public string Name { get; set; }
+        public string Currency { get; set; }
+        public int Deleted { get; set; }
+
+    }
+}
